32dd82a7604689814d11b7d991a261dead8f4380
AryanKRidesIq <aryan@ridesiq.com>
2025-02-04 19:15:21 +0530

    Add : Carousel Logic as well as some demo assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
-
-import AnimatedLogoCloud from "@/components/custom/Carousel";
+import MarqueeEffect from "@/components/custom/Carousel";
 import HeroSection from "@/components/custom/HeroSection";
 import { WorldMapDemo } from "@/components/custom/WorldmapDemo";
 
 // import { TextEffect } from "@/components/ui/text-effect";
 
-
 const page = ({}) => {
-    return (<>
-        <HeroSection />
-        <AnimatedLogoCloud/>
-        <WorldMapDemo />
-        
-        
-        </>
+	return (
+		<>
+			<HeroSection />
+			<MarqueeEffect baseVelocity={-3}>
+                <div className="flex flex-col items-center justify-center w-96 bg-red-200 h-40">1</div>
+                <div className="flex flex-col items-center justify-center w-96 bg-red-300 h-40">2</div>
+                <div className="flex flex-col items-center justify-center w-96 bg-red-400 h-40">3</div>
+                <div className="flex flex-col items-center justify-center w-96 bg-red-500 h-40">4</div> 
+                <div className="flex flex-col items-center justify-center w-96 bg-red-600 h-40">5</div>
+			</MarqueeEffect>
+			<WorldMapDemo />
+		</>
 	);
 };
 
--- src/components/custom/Carousel.tsx
+++ src/components/custom/Carousel.tsx
@@ -1,77 +1,67 @@
-"use client"
-import React from "react";
-import { motion } from "framer-motion";
+"use client";
 
-const logos = [
-	{
-		Element: <h1 className="text-red-500">hello world</h1>,
-	},
-	{
-		Element: <h1 className="text-4xl"> hello bb? </h1>,
-	},
-	{
-		Element: (
-			<div className="flex flex-col gap-2">
-				{[0, 1].map((key) => (
-					<div
-						className="flex flex-col items-center justify-center w-4 h-4  bg-indigo-300"
-						key={key}
-					>
-						<h1 className="text-4xl"> hello bb? </h1>
-					</div>
-				))}
-			</div>
-		),
-	},
-	{
-		Element: <h1 className="text-4xl"> hello bb? </h1>,
-	},
-	{
-		Element: <h1 className="text-4xl"> hello bb? </h1>,
-	},
-	{
-		Element: <h1 className="text-4xl"> hello bb? </h1>,
-	},
-	{
-		Element: <h1 className="text-4xl"> hello bb? </h1>,
-	},
-];
+import React, { useRef, type ReactNode } from "react";
+import { motion, useScroll, useSpring, useTransform, useMotionValue, useVelocity, useAnimationFrame } from "framer-motion";
+import { wrap } from "@motionone/utils";
+import { cn } from "@/lib/utils";
 
-const AnimatedLogoCloud = () => {
-	return (
-		<div className="w-full py-2 bg-black text-white">
-			<div className="mx-auto w-full px-4 md:px-8">
-				<div
-					className="group relative mt-6 flex gap-6 overflow-hidden p-2"
-					style={{
-						maskImage: "linear-gradient(to left, transparent 0%, black 20%, black 80%, transparent 95%)",
-					}}
-				>
-					{[1, 2, 3, 4, 5].map((index) => (
-						<motion.div
-							key={index}
-							className="flex shrink-0 flex-row justify-around gap-20"
-							animate={{
-								x: [0, "-100%"],
-								transition: {
-									duration: 30,
-									ease: "linear",
-									repeat: Infinity,
-									delay: (index - 1) * 6,
-								},
-							}}
-						>
-                            {logos.map((Componts, key) => (
-                                <div className="flex items-center justify-center w-40 h-96  " key={key}>
-                                    {Componts.Element}
-                                </div>
-							))}
-						</motion.div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+type MarqueeAnimationProps = {
+	children: ReactNode;
+	className?: string;
+	direction?: "left" | "right";
+	baseVelocity: number;
 };
 
-export default AnimatedLogoCloud;
+export default function MarqueeAnimation({ children, className, direction = "left", baseVelocity = 10 }: MarqueeAnimationProps) {
+	const baseX = useMotionValue(0);
+	const { scrollY } = useScroll();
+	const scrollVelocity = useVelocity(scrollY);
+	const smoothVelocity = useSpring(scrollVelocity, {
+		damping: 50,
+		stiffness: 400,
+	});
+	const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 0], {
+		clamp: false,
+	});
+
+	const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+	const directionFactor = useRef<number>(1);
+	useAnimationFrame((t, delta) => {
+		let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+		if (direction == "left") {
+			directionFactor.current = 1;
+		} else if (direction == "right") {
+			directionFactor.current = -1;
+		}
+
+		moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+		baseX.set(baseX.get() + moveBy);
+	});
+
+	const content = React.Children.toArray(children);
+
+	return (
+		<div className="overflow-hidden max-w-[100vw] text-nowrap flex-nowrap flex relative">
+			<motion.div
+				className={cn("font-bold uppercase text-5xl flex flex-nowrap text-nowrap", className)}
+				style={{ x }}
+			>
+				{[...Array(4)].map((_, index) => (
+					<React.Fragment key={index}>
+						{React.Children.map(content, (child, childIndex) => (
+							<div
+								key={childIndex}
+								className="me-10"
+							>
+								{child}
+							</div>
+						))}
+					</React.Fragment>
+				))}
+			</motion.div>
+		</div>
+	);
+}
