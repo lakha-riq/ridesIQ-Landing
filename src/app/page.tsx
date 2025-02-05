1a346dd368a7d01bf82df5eeb156bb719046cbf8
AryanKRidesIq <aryan@ridesiq.com>
2025-02-05 11:20:18 +0530

    Add: Images and stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MarqueeEffect from "@/components/custom/Carousel";
 import HeroSection from "@/components/custom/HeroSection";
 import { WorldMapDemo } from "@/components/custom/WorldmapDemo";
+import Image from "next/image";
 
 // import { TextEffect } from "@/components/ui/text-effect";
 
@@ -8,13 +9,29 @@ const page = ({}) => {
 	return (
 		<>
 			<HeroSection />
-			<MarqueeEffect baseVelocity={-3}>
-                <div className="flex flex-col items-center justify-center w-96 bg-red-200 h-40">1</div>
-                <div className="flex flex-col items-center justify-center w-96 bg-red-300 h-40">2</div>
-                <div className="flex flex-col items-center justify-center w-96 bg-red-400 h-40">3</div>
-                <div className="flex flex-col items-center justify-center w-96 bg-red-500 h-40">4</div> 
-                <div className="flex flex-col items-center justify-center w-96 bg-red-600 h-40">5</div>
+			<MarqueeEffect baseVelocity={-2}>
+				<div className="flex flex-col items-center justify-center w-[50rem]  h-96 rounded-lg">
+					<Image src="/assets/1.png" alt="rides" className="w-full h-full object-cover rounded-lg" width={500} height={5000} />
+				</div>
+				<div className="flex flex-col items-center justify-center w-40 bg-red-300 h-96 rounded-lg">
+					<Image src="/assets/6.jpg" alt="rides" className="w-full h-full object-cover rounded-lg" width={500} height={5000} />
+				</div>
+				<div className="flex flex-col items-center justify-center w-[30rem] bg-red-400 h-96 rounded-lg">
+					<Image src="/assets/2.png" alt="rides" className="w-full h-full object-cover rounded-lg" width={500} height={500} />
+				</div>
+				<div className="flex flex-col items-center justify-center w-80 bg-red-500 h-96 rounded-lg">
+					<Image src="/assets/3.png" alt="rides" className="w-full h-full object-cover rounded-lg" width={500} height={500} />
+				</div>
+				<div className="flex flex-col items-center justify-center w-80 h-96 rounded-lg gap-6">
+					<div className="bg-red-600 rounded-lg h-[12rem] w-full flex items-center justify-center">
+						<Image src="/assets/4.png" alt="rides" className="w-full h-full object-cover rounded-lg" width={250} height={250} />
+					</div>
+					<div className="bg-red-700 rounded-lg h-[12rem] w-full flex items-center justify-center">
+						<Image src="/assets/5.png" alt="rides" className="w-full h-full object-cover rounded-lg" width={250} height={250} />
+					</div>
+				</div>
 			</MarqueeEffect>
+			<div className="flex flex-col items-center justify-center w-full min-h-screen" >the world is watching us grow </div>
 			<WorldMapDemo />
 		</>
 	);
