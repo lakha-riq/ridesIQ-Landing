06ed6989622dc27a99658a525da5953a33f181ff
AryanKRidesIq <aryan@ridesiq.com>
2025-02-06 22:22:47 +0530

    Testimonials section basic done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { BentoGridDemo } from "@/components/custom/Grid";
 import HeroSection from "@/components/custom/HeroSection";
 import Quotes from "@/components/custom/Quotes";
+import { TestimonialsSectionDemo } from "@/components/custom/TestimonialsAll";
 import { WorldMapDemo } from "@/components/custom/WorldmapDemo";
 
 // import { TextEffect } from "@/components/ui/text-effect";
@@ -20,7 +21,7 @@ const page = ({}) => {
 				quote="We are a team of experts in the field of IoT "
       />
       <div>
-        
+        <TestimonialsSectionDemo/>
       </div>
 			<WorldMapDemo />
 		</>
