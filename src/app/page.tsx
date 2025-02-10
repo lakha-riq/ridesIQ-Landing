fa1e13939b2ac6adada88aa3f9e3490401169a17
AryanKRidesIq <aryan@ridesiq.com>
2025-02-10 22:05:02 +0530

    Gradient Card add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import GradientCard from "@/components/custom/GradientCard";
 import { BentoGridDemo } from "@/components/custom/Grid";
 import HeroSection from "@/components/custom/HeroSection";
 import Quotes from "@/components/custom/Quotes";
@@ -11,18 +12,20 @@ const page = ({}) => {
 	return (
 		<>
 			<HeroSection />
-      <div className="flex flex-col items-center justify-center min-w-full min-h-[50vh] pt-20">
-        <div className="text-3xl">Discover why people are willing to work with us</div>
-        <div className="text-xl opacity-50">the features and benefits of our services</div>
+			<div className="flex flex-col items-center justify-center min-w-full min-h-[50vh] pt-20">
+				<div className="text-3xl">Discover why people are willing to work with us</div>
+				<div className="text-xl opacity-50">the features and benefits of our services</div>
 				<BentoGridDemo />
 			</div>
 			<Quotes
 				header="Working with Industry leading Infrastructure"
 				quote="We are a team of experts in the field of IoT "
-      />
-      <div>
-        <TestimonialsSectionDemo/>
-      </div>
+			/>
+			<div>
+				<TestimonialsSectionDemo />
+			</div>
+
+				<GradientCard />
 			<WorldMapDemo />
 		</>
 	);
