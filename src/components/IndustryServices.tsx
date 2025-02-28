472a6fa19ff5fa422e4c4ffcea53dee4fab0db9d
AryanKRidesIq <aryan@ridesiq.com>
2025-02-28 22:12:10 +0530

    Centering :)

--- src/components/IndustryServices.tsx
+++ src/components/IndustryServices.tsx
@@ -17,7 +17,7 @@ const IndustryServices = ({}) => {
 					{/* Hard-Wired Tracker Card */}
 					<div className="rounded-3xl overflow-hidden relative h-[450px] group transition-transform hover:scale-105">
 						<div className="absolute inset-0 bg-gradient-to-b from-[#678FCA] to-[#4A6491]" />
-						<div className="absolute right-20 left-100 -rotate-90  bottom-120 translate-x-1/2 w-[280px] h-[280px]">
+						<div className="absolute -left-24 top-0 -rotate-90  translate-x-1/2 w-[280px] h-[280px]">
 							<Image
 								src="/assets/devices/device1.svg"
 								alt="Hard-Wired Tracker"
@@ -36,7 +36,7 @@ const IndustryServices = ({}) => {
 					{/* OBD Tracker Card */}
 					<div className="rounded-3xl overflow-hidden relative h-[450px] group transition-transform hover:scale-105">
 						<div className="absolute inset-0 bg-gradient-to-b from-[#678FCA] to-[#99D5C9]" />
-						<div className="absolute right-0 left-100 top-32  translate-x-1/2 w-[280px] h-[280px]">
+						<div className="absolute -left-10 top-32  translate-x-1/2 w-[280px] h-[280px]">
 							<Image
 								src="/assets/devices/OBD.svg"
 								alt="OBD Tracker"
@@ -55,7 +55,7 @@ const IndustryServices = ({}) => {
 					{/* Asset Tracker Card */}
 					<div className="rounded-3xl overflow-hidden relative h-[450px] group transition-transform hover:scale-105">
 						<div className="absolute inset-0 bg-gradient-to-b from-[#99D5C9] to-[#7AB3A9]" />
-						<div className="absolute right-0 left-100 top-20  translate-x-1/2 w-[280px] h-[280px]">
+						<div className="absolute -left-10 top-12  translate-x-1/2 w-[280px] h-[280px]">
 							<Image
 								src="/window.svg"
 								alt="Asset Tracker"
